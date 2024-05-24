620e154e7b4d675065d16ab2ea92020fbf9ce6c6
mwsa2 <114543550+mwsa2@users.noreply.github.com>
2024-05-23 21:29:38 -0300

    Add files via upload

---
+++ aula1.hs
@@ -0,0 +1,29 @@
+square :: Int -> Int 
+square x = x * x
+
+
+allEqual :: Int -> Int -> Int -> Bool
+allEqual a b c = (a == b) && (b == c)
+
+all4Equal :: Int -> Int -> Int -> Int -> Bool
+all4Equal a b c d = allEqual a b c && d == a 
+
+EqualCount 
+
+maxi :: Int -> Int -> Int
+maxi a b | a >= b = a
+         | otherwise = b  
+
+vendas :: Int -> Int
+vendas 0 = 10
+vendas 1 = 20
+vendas 2 = 30
+
+
+totalVendas :: Int -> Int
+totalVendas 0 = vendas 0
+totalVendas n = vendas n + totalVendas (n-1)
+
+fat :: Int -> Int
+fat 1 = 1
+fat a = a * fat (a-1)
