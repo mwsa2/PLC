aa7b1b2a746e6a30c882e2303b4b44a50df53072
mwsa2 <114543550+mwsa2@users.noreply.github.com>
2024-08-23 16:13:56 -0300

    Update hashlog1.c

--- hashlog1.c
+++ hashlog1.c
@@ -8,11 +8,25 @@ typedef struct _conexao
     unsigned int cliente;
 } Conexao;
 
-void adicionarConexao(unsigned int timestamp, unsigned int cliente){
-    Conexao *con = (Conexao *)malloc(sizeof(Conexao));
-    con->timestamp = timestamp;
-    con->cliente = cliente;
+int adicionarConexaoOrd(unsigned int timestamp, unsigned int cliente, Conexao *conexoes, int *tam){
+    int pos = 0;
+    printf("%d\n", *tam);
+    conexoes = (Conexao*)realloc(conexoes, (*tam) * sizeof(Conexao));
+    printf("OK\n");  
+    if (conexoes == NULL) {
+        printf("Reallocation Failed\n");
+        exit(0);
+    }
     
+    conexoes[*tam]->timestamp = timestamp;
+    conexoes[*tam]->cliente = cliente;
+    //*tam = *tam + 1;
+    
+    for(int i=0; i<*tam; i++){
+        printf("%u %u\n", conexoes[i]->timestamp, conexoes[i]->cliente);
+    }
+
+    return pos;
 }
 
 int main()
@@ -20,9 +34,13 @@ int main()
     // Inicio
     char input[30];
     char command[4];
-    unsigned int client;
+    unsigned int cliente;
+    unsigned int timestamp;
     int continuar = 1;
+    int tam = 1;
+    int posConexao;
     //
+    Conexao *conexoes; //= (Conexao *)malloc(1 * sizeof(Conexao));
     while (continuar)
     {
         scanf(" %29[^\n]s", &input);
@@ -33,23 +51,22 @@ int main()
             sscanf(input, "%s ", &command);
             if (strcmp(command, "NEW") == 0)
             {   
-                sscanf(input, "%s %s %d", &command, &nome, &pontuacao);
-
-                Nome *searchNome = AVLsearchNome(rootNome, nomeAux);
-                if (searchNome != NULL)
-                    printf("%s ja esta no sistema.\n", nome);
-                else{
-                    rootNome = inserirNome(rootNome, nomeAux);
-                    rootJogador = inserirJogador(rootJogador, nomeAux, pontuacao);
-                    printf("%s inserido com sucesso!\n", nome);
-                }
+                sscanf(input, "%s %u %u", &command, &timestamp, &cliente);
+                adicionarConexaoOrd(timestamp, cliente, conexoes, &tam);
+                
             }
             else if (strcmp(command, "QRY") == 0)
             {
-                sscanf(input, "%s %d", &command, &pontuacao);
+                
             }
         }
     }
-            
+
+    /*for(int i=0; i<tam; i++){
+        printf("tam: %d\n", tam);
+        printf("i: %d\n", i);
+        printf("%u %u\n", conexoes[i].timestamp, conexoes[i].cliente);
+    }*/
+    free(conexoes);
     return 0;
 }
