7e5c16c7755b22df38390789269f0fc1d0baba3a
mwsa2 <114543550+mwsa2@users.noreply.github.com>
2024-08-24 16:01:32 -0300

    Update hashlog1.c

--- hashlog1.c
+++ hashlog1.c
@@ -1,6 +1,6 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct _conexao
 {
@@ -8,77 +8,81 @@ typedef struct _conexao
     unsigned int cliente;
 } Conexao;
 
-int adicionarConexaoOrd(unsigned int timestamp, unsigned int cliente, Conexao *conexoes, int *tam){
-    int pos = 0;
-    printf("count %d\n", *tam);
-    
-	if(*tam == 0){
-		conexoes = (Conexao *)malloc(sizeof(Conexao));
-		if (conexoes == NULL) {
-			printf("memory cannot be allocated malloc");
-			exit(0);
-		}
-	}else{
-		printf(" novo tam %d\n", *tam);
-		conexoes = (Conexao*)realloc(conexoes, 1 * sizeof(Conexao));
-		if (conexoes == NULL) {
-			printf("memory cannot be allocated realloc");
-			exit(0);
-		}
-	}
-	
-	printf("OK\n");  
-    
-    //conexoes[*tam].timestamp = timestamp;
-    //conexoes[*tam].cliente = cliente;
-    *tam = *tam + 1;
-    
-    /*for(int i=0; i<*tam; i++){
-		printf("i: %d\n", i);
-		printf("%u %u\n", conexoes[i].timestamp, conexoes[i].cliente);
-    }*/
 
+int adicionarConexaoOrd(unsigned int timestamp, unsigned int cliente, Conexao *conexoes, int *index){
+    int pos = 1;
+    //
+    conexoes[*index].timestamp = timestamp;
+    conexoes[*index].cliente = cliente;
+    
+    /*printf("would you like to add more(1/0): ");
+    scanf("%d", &ans);
+    if (ans == 1) {
+        index++;
+        conexoes = (Conexao*)realloc(conexoes, (index + 1) * sizeof(Conexao)); 
+        if (conexoes == NULL) {
+            printf("memory cannot be allocated");
+            exit(0);
+        }
+    }*/
     return pos;
 }
 
 int main()
 {
-    // Inicio
+    Conexao *conexoes;
     char input[30];
-    char command[4];
+    char command[4]; 
+    int index = 0;
+    int ans;
     unsigned int cliente;
     unsigned int timestamp;
     int continuar = 1;
-    int tam = 0;
-    int posConexao;
-    //
-    Conexao *conexoes;// = (Conexao *)malloc(sizeof(Conexao));
-    while (continuar)
-    {
-        scanf(" %29[^\n]s", &input);
-        //
-        continuar = strcmp(input, "END");
-        if (continuar)
-        {
-            sscanf(input, "%s ", &command);
-            if (strcmp(command, "NEW") == 0)
-            {   
-                sscanf(input, "%s %u %u", &command, &timestamp, &cliente);
-                adicionarConexaoOrd(timestamp, cliente, conexoes, &tam);
-                
-            }
-            else if (strcmp(command, "QRY") == 0)
-            {
-                
-            }
-        }
-    }
 
-    /*for(int i=0; i<tam; i++){
-        printf("tam: %d\n", tam);
-        printf("i: %d\n", i);
-        printf("%u %u\n", conexoes[i].timestamp, conexoes[i].cliente);
-    }*/
-    free(conexoes);
+    conexoes = (Conexao*)malloc(sizeof(Conexao)); 
+    if (conexoes == NULL) {
+        printf("memory cannot be allocated");
+        exit(0);
+    }
+    else {
+        while(continuar) {
+            
+            scanf(" %29[^\n]s", &input);
+            //
+            continuar = strcmp(input, "END");
+            if (continuar)
+            {
+                sscanf(input, "%s ", &command);
+                if (strcmp(command, "NEW") == 0)
+                {   
+                    sscanf(input, "%s %u %u", &command, &timestamp, &cliente);
+                    adicionarConexaoOrd(timestamp, cliente, conexoes, &index);
+                    /*conexoes[index].timestamp = timestamp;
+                    conexoes[index].cliente = cliente;
+                    
+                    printf("would you like to add more(1/0): ");
+                    scanf("%d", &ans);
+                    if (ans == 1) {
+                        index++;
+                        conexoes = (Conexao*)realloc(conexoes, (index + 1) * sizeof(Conexao)); 
+                        if (conexoes == NULL) {
+                            printf("memory cannot be allocated");
+                            exit(0);
+                        }
+                    }*/
+                    
+                }
+                else if (strcmp(command, "QRY") == 0)
+                {
+                    
+                }
+            }
+        };
+        
+        for (int i = 0; i <= index; i++) {
+            printf("conexoes %d are: %u\n ", i, conexoes[i].cliente);
+        }
+        free(conexoes);
+    }
     return 0;
 }
