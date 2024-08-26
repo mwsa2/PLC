c9f5da834a80b1b123ef1afc094e422476297629
mwsa2 <114543550+mwsa2@users.noreply.github.com>
2024-08-26 20:09:03 -0300

    Update hashlog1.c

--- hashlog1.c
+++ hashlog1.c
@@ -20,23 +20,17 @@ int adicionarConexaoOrd(unsigned int timestamp, unsigned int cliente, HashTable 
     int pos = 0;
     //
     Conexao *conexoes;
-    //printf("add....\n");
     if(hashTable[*hash].conexoes == NULL){
         conexoes = (Conexao*)malloc(100000 * sizeof(Conexao)); 
         if (conexoes == NULL) {
             printf("memory cannot be allocated");
             exit(0);
         }
-        //printf("primeiro....\n");
         conexoes[0].timestamp = timestamp;
         conexoes[0].cliente = cliente;
-        //
-        //printf("Ok1\n");
         hashTable[*hash].tam = 1;
         hashTable[*hash].conexoes = conexoes;
-        //printf("Ok2\n");
     }else{
-        //printf("ja tem....\n");
         pos = hashTable[*hash].tam;
         conexoes = hashTable[*hash].conexoes;
         while (pos > 0 && conexoes[pos - 1].timestamp > timestamp)
@@ -44,14 +38,12 @@ int adicionarConexaoOrd(unsigned int timestamp, unsigned int cliente, HashTable 
             conexoes[pos] = conexoes[pos - 1];
             pos--;
         }
-        //printf("OK3\n");
         conexoes[pos].timestamp = timestamp;
         conexoes[pos].cliente = cliente;
         hashTable[*hash].tam = hashTable[*hash].tam + 1;
-        //printf("OK4\n");
     }
     *insersoesHash = *insersoesHash + 1;
-    printf("%d %d\n", *hash, hashTable[*hash].tam);
+    pos = *hash;
     return pos;
 }
 
@@ -106,68 +98,38 @@ int main()
                 sscanf(input, "%s %u %u", &command, &timestamp, &cliente);
                 hash = (timestamp % m);
                 fator = ((float)insersoesHash / (float)m);
-                if( fator <= Lmax)
-                    adicionarConexaoOrd(timestamp, cliente, hashTable, &hash, &insersoesHash);                    
-                else{
+                if( fator <= Lmax){
+                    posCliente = adicionarConexaoOrd(timestamp, cliente, hashTable, &hash, &insersoesHash);                    
+                    printf("%d %d\n", posCliente, hashTable[posCliente].tam);
+                }else{
+                    int mLast = m;
                     m = (2*m) + 1;
-                    hashTable = (HashTable*)realloc(hashTable, m * sizeof(HashTable));
-                    if (hashTable == NULL) {
-                        printf("realloc error");
+                    HashTable *hashTableNew;
+                    hashTableNew = (HashTable*)malloc(m * sizeof(HashTable)); 
+                    if (hashTableNew == NULL) {
+                        printf("memory cannot be allocated hashTableNew\n");
                         exit(0);
                     }
-                    for(int i = 0; i < m; i++){
-                        hashTable[i].hashing = 1;
-                    }
+                    hashTableNew->conexoes = NULL;
+                    insersoesHash = 0;
                     //reordena
-                    printf("reordernar\n");
-                    for(int i = 0;i < m; i++){
-                        if(hashTable[i].conexoes != NULL){
-                            hash = (hashTable[i].conexoes[0].timestamp % m);
-                            printf("inserindo fila: %d na posicao: %d\n", i, hash);
-                            //verifica se o novo hash ja tem arquivo;
-                            if(hashTable[hash].conexoes == NULL){
-                               printf("%d livre\n", hash);     
-                               hashTable[hash].conexoes = hashTable[i].conexoes;
-                               free(hashTable[i].conexoes);
-                               printf("reordernar OK e conexoes %d free\n", i);      
-                            }else{
-                                printf("%d ocupada com %d\n", hash, hashTable[hash].conexoes[0].cliente);
-                                if(hashTable[hash].hashing == 0 ){ 
-                                    printf("%d jah foi atualizada, inserindo os elemnetos de %d\n", hash, i);
-                                    for(int j=0; j<hashTable[i].tam; j++){
-                                        adicionarConexaoOrd(hashTable[i].conexoes[j].timestamp, hashTable[i].conexoes[j].cliente, hashTable, &hash, &insersoesHash);  
-                                    }   
-                                    free(hashTable[i].conexoes);
-                                }else{
-                                    printf("%d nao foi atualizada\n", hash);
-                                    printf("copiando hash %d para temp\n", hash);
-                                    Conexao *conexoesTemp;
-                                    conexoesTemp = hashTable[hash].conexoes;
-                                    printf("copiando i:%d para hash %d\n", i, hash);
-                                    hashTable[hash].conexoes = hashTable[i].conexoes;
-                                    hashTable[hash].hashing = 0;
-                                    printf("copias OK\n");
-                                    //
-                                    hash = (conexoesTemp[0].timestamp % m);
-                                    printf("inserindo temp na hash:%d\n", hash);
-                                    hashTable[hash].conexoes = conexoesTemp;
-                                    hashTable[hash].hashing = 0;
-                                    printf("inserindo temp OK\n");
-                                    free(conexoesTemp);
-                                    printf("free temp\n");
-                                }
-                            }
-                            
-                        }
+                    for(int i = 0;(i < mLast) && (hashTable[i].conexoes != NULL); i++){
+                        for(int j=0; j<hashTable[i].tam; j++ ){
+                            hash = (hashTable[i].conexoes[j].timestamp % m);
+                            adicionarConexaoOrd(hashTable[i].conexoes[j].timestamp, hashTable[i].conexoes[j].cliente, hashTableNew, &hash, &insersoesHash);          
+                        }       
                     }
-                    //adiciona
-                    printf("tentando adicionar o %u\n", timestamp);
-                    adicionarConexaoOrd(timestamp, cliente, hashTable, &hash, &insersoesHash); 
+                    free(hashTable);
+                    hashTable = hashTableNew;
+                    hash = (timestamp % m);
+                    posCliente = adicionarConexaoOrd(timestamp, cliente, hashTable, &hash, &insersoesHash);
+                    printf("%d %d\n", posCliente, hashTable[posCliente].tam); 
                 }    
                     
             }
             else if (strcmp(command, "QRY") == 0)
             {
+                posCliente = -1;
                 sscanf(input, "%s %u", &command, &timestamp);
                 hash = (timestamp % m);
                 if(hashTable[hash].tam > 0)
